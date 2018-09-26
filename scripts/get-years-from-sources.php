efacc49220b6d5bcfe0baefc87c002b540313a68
mmmenno <menno@denengelse.nl>
2018-09-26 11:26:58 +0200

    import bag building years

--- scripts/get-years-from-sources.php
+++ scripts/get-years-from-sources.php
@@ -61,6 +61,26 @@ if (($handle = fopen("../sources/raadsbesluiten.csv", "r")) !== FALSE) {
     fclose($handle);
 }
 
+
+// BAG BUILDING YEARS
+
+echo "\n\ninsert BAG buildingyears";
+if (($handle = fopen("../sources/BAG-oldest-building-of-street.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        if($check = streetUriPresent($data[0])){
+            $result = $mysqli->query(   "update asap_years 
+                                        set earliest_bag_building_year = " . $data[3] . " 
+                                        where street_uri = '" . $data[0] . "'");
+            echo " .";
+        }
+    }
+    fclose($handle);
+}
+
+
+
+
+
 function streetUriPresent($uri){
     global $mysqli;
 
@@ -73,55 +93,7 @@ function streetUriPresent($uri){
     return true;
 }
 
-die;
-while($row = $result->fetch_assoc()){
-    
-    $s2 = "select * from adamnet_hulptabellen.nwbwegvakken 
-            where naam = '" . $mysqli->real_escape_string($row['preflabel']) . "'";
-        
-        
-    $r2 = $mysqli->query($s2);
 
-    if($r2->num_rows > 0){
-
-        $line = array();
-        if($r2->num_rows ==1){
-            $row2 = $r2->fetch_assoc();
-            $line = json_decode($row2['geom'],true);
-        }else{
-            $line['type'] = "MultiLineString";
-            $line['coordinates'] = array();
-            while($row2 = $r2->fetch_assoc()){
-                $geom = json_decode($row2['geom'],true);
-                $line['coordinates'][] = $geom['coordinates'];
-            }
-        }
-        
-        
-        echo $c($row['sources'] . " " . $row['preflabel'] . " found")->green() . "\n";
-        $upd = "insert into streetgeometries (street_identifier, geojson, sources) values (
-                " . $row['id'] . ",
-                '" . json_encode($line) . "',
-                'nwb-25-9-18'
-                )";
-
-        if($run){
-            if (!$mysqli->query($upd)) {
-                printf("Error: %s\n", $mysqli->error);
-            }
-            echo " +";
-        }else{
-            echo $upd . "\n";
-        }
-    }else{
-        echo $c($row['sources'] . " " . $row['preflabel'] . " not found")->red() . "\n";
-        //echo $c(" -")->red();
-    }
-
-    
-
-
-}
 
 
 ?>
